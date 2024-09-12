721e590a67a4301645b6853b82280d93ff8eeedf
elyxiaaa <oquinianoa@gmail.com>
2024-09-13 00:37:42 +0800

    Edited: background components

--- src/Components/Budgeting.jsx
+++ src/Components/Budgeting.jsx
@@ -3,9 +3,9 @@ function Budgeting () {
 
     return(
         <>
-            <div className="h-full w-screen bg-BG bg-no-repeat bg-center">
+            <div className="h-full">
                 <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center">
-                    <h1 className="text-base-100 text-4xl">Budgeting</h1>
+                    <h1 className="text-black text-4xl">Budgeting</h1>
                 </div>
             </div>
         </>
--- src/Components/vendorInvoice.jsx
+++ src/Components/vendorInvoice.jsx
@@ -2,9 +2,9 @@
 function vendorInvoice () {
     return (
         <>
-        <div className="h-full w-screen bg-BG bg-no-repeat bg-cover">
+        <div className="h-full">
             <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center">
-                <h1 className="text-base-100 text-4xl">Vendor Invoice</h1>
+                <h1 className="text-black text-4xl">Vendor Invoice</h1>
             </div>
         </div>
         </>
