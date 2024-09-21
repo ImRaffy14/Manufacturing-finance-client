b3e9ba994b0b24b69c811a48a7dc417f740d14b2
elyxiaaa <oquinianoa@gmail.com>
2024-09-22 00:19:16 +0800

    Edited: viewAll accounts

--- src/Components/viewAllAccounts.jsx
+++ src/Components/viewAllAccounts.jsx
@@ -108,15 +108,6 @@ function ViewAllAccounts() {
           <>
             {/* Edit Mode */}
             <div>
-              <label className="block text-gray-600 font-medium mb-1">Image URL:</label>
-              <input
-                name="image"
-                value={editData.image}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              />
-            </div>
-            <div>
               <label className="block text-gray-600 font-medium mb-1">Full Name:</label>
               <input
                 name="fullName"
