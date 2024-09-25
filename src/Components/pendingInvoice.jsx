ce0e7690f6e4b0195a9cfa9367d208fee7c021e2
elyxiaaa <oquinianoa@gmail.com>
2024-09-26 01:25:04 +0800

    Added: Invoice download, improved preview

--- src/Components/pendingInvoice.jsx
+++ src/Components/pendingInvoice.jsx
@@ -15,8 +15,8 @@ function pendingInvoice() {
       const canvas = await html2canvas(inputData);
       const imgData = canvas.toDataURL("image/png");
 
-      const customWidth = 500;  // Adjust this value to set your custom width
-      const customHeight =700; // Adjust this value to set your custom height
+      const customWidth = 700;  // Adjust this value to set your custom width
+      const customHeight = 700; // Adjust this value to set your custom height
 
       const pdf = new jsPDF({
         orientation: "portrait",
@@ -111,10 +111,6 @@ function pendingInvoice() {
       value.toString().toLowerCase().includes(searchText.toLowerCase())
     )
   );
-  const confirmSubmission = () => {
-    socket.emit("create_invoice", pendingInvoiceData);
-  };
-
   
   // Handle row click to show modal
   const handleRowClick = (row) => {
@@ -190,26 +186,152 @@ function pendingInvoice() {
          {/* Modal for displaying row data */}
       {selectedRowData && (
         <dialog id="row_modal" className="modal">
-          <div className="modal-box">
-            
-          <div ref={invoiceRef} className="invoice-container bg-white p-8 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4">Pending Invoice</h2>
-        <div className="mb-4">
-          <h3 className="text-lg font-semibold">Customer Information</h3>
-          <p><strong>Customer ID:</strong> {selectedRowData.customerId}</p>
-          <p><strong>Name:</strong> {selectedRowData.customerName}</p>
-          <p><strong>Contact:</strong> {selectedRowData.customerContact}</p>
+          <div className="modal-box  w-full h-full max-w-[1000px]">
+            <div className="invoice-container bg-white p-8 rounded-lg shadow-md">
+              <div className="flex justify-between">
+                <div className="text-left">
+                  <h2 className="text-4xl  font-semibold mb-4"><strong>{selectedRowData.Status}</strong></h2>
+                </div>
+                <div className="text-right ">
+              <p><strong>Create Date:</strong> {selectedRowData.createdAt || '01234'}</p>
+              <p><strong>Date Updated:</strong> {selectedRowData.updatedAt || '11.02.2030'}</p>
+            </div>
         </div>
-        <div className="mb-4">
-          <h3 className="text-lg font-semibold">Invoice Information</h3>
-          <p><strong>Invoice ID:</strong> {selectedRowData._id}</p>
-          <p><strong>Invoice Date:</strong> {selectedRowData.invoiceDate}</p>
-          <p><strong>Due Date:</strong> {selectedRowData.dueDate}</p>
+
+        <div className="mt-10">
+          <div className="flex justify-between">
+            <div className="text-left">
+              {/* cUSTOMER INFO */}
+              <h3 className="text-lg font-semibold"><strong>Customer Information</strong></h3>
+              <p><strong>Customer ID:</strong> {selectedRowData.customerId}</p>
+              <p><strong>Name:</strong> {selectedRowData.customerName}</p>
+              <p><strong>Contact:</strong> {selectedRowData.customerContact}</p>
+              <p><strong>Address:</strong> {selectedRowData.customerAddress}</p>
+            </div>
+          
+            <div className="text-right">
+          {/* Invoice Info */}
+          <h3 className="text-lg font-semibold"><strong>Invoice Information</strong></h3>
+            <p><strong>INVOICE ID:</strong> {selectedRowData._id || '01234'}</p>
+            <p><strong>DATE:</strong> {selectedRowData.invvoiceDate || '11.02.2030'}</p>
+            <p><strong>DUE DATE:</strong> {selectedRowData.dueDate || '11.03.2030'}</p>
+            </div>
+            </div>
+
+             {/* Shipping Information */}
+          <h3 className="text-lg font-bold mt-5 font-semibold"><strong>Delivery Information</strong></h3>
+            <p><strong>Shipping Method:</strong> {selectedRowData.shippingMethod || '01234'}</p>
+            <p><strong>Delivery Date:</strong> {selectedRowData.deliveryDate || '11.02.2030'}</p>
+
+            {/* TERMS AND NOTES */}
+          <h3 className="text-lg font-bold mt-5 font-semibold"><strong>Payment Terms</strong></h3>
+            <p><strong>Payment Terms:</strong> {selectedRowData.terms || '01234'}</p>
+            <p><strong>Note:</strong> {selectedRowData.note || 'Dilaan mo ang burat kong makatas'}</p>
         </div>
-        <div className="mb-4">
-          <h3 className="text-lg font-semibold">Additional Information</h3>
-          <p><strong>Payment Terms:</strong> {selectedRowData.paymentTerms}</p>
+        
+
+        <div className="mt-10">
+          
+          {/* Order Info */}
+          <h3 className="text-lg font-bold mt-5 font-semibold"><strong>Order Information</strong></h3>
+          <div className="flex flex-col">
+            <p><strong>Order Number:</strong> {selectedRowData.orderNumber || '01234'}</p>
+            <p><strong>Order Date:</strong> {selectedRowData.orderDate || '11.02.2030'}</p>
+            </div>
+
+          <table className="w-full text-left table-auto border-collapse">
+            <thead>
+              <tr className="border-b">
+                <th className="py-2">DESCRIPTION</th>
+                <th className="py-2 text-right">UNIT PRICE</th>
+                <th className="py-2 text-right">QTY</th>
+                <th className="py-2 text-right">TOTAL</th>
+              </tr>
+            </thead>
+            <tbody>
+              {selectedRowData.items && selectedRowData.items.map((item, index) => (
+                <tr key={index} className="border-b">
+                  <td className="py-2">{item.itemName}</td>
+                  <td className="py-2 text-right">{formatCurrency(item.price)}</td>
+                  <td className="py-2 text-right">{item.quantity}</td>
+                  <td className="py-2 text-right">
+                    {formatCurrency(item.price * item.quantity)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+
+        {/* Subtotal, Discounts, and Total */}
+        <div className="mt-8">
+          <div className="grid grid-cols-2 gap-2">
+            <div className="text-right">
+              <p><strong>SUBTOTAL:</strong></p>
+              <p><strong>DISCOUNTS:</strong></p>
+            </div>
+            <div className="text-right">
+              <p>{formatCurrency(selectedRowData.subTotal || 0)}</p>
+              <p>{formatCurrency(selectedRowData.discounts || 0)}</p>
+            </div>
+          </div>
+
+          <div className="border-t-2 border-gray-300 my-2"></div>
+
+          <div className="grid grid-cols-2 gap-2">
+            <div className="text-right">
+              <p className="text-lg font-bold"><strong>TOTAL AMOUNT:</strong></p>
+            </div>
+            <div className="text-right">
+              <p className="text-lg font-bold">{formatCurrency(selectedRowData.totalAmount || 0)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+        {/* INvoice ref */}
+        <div className="container mx-auto p-4">
+      {/* Invoice layout */}
+      <div ref={invoiceRef}
+        style={{
+          position: 'absolute',
+          top: '-9999px',
+          left: '-9999px',
+          visibility: 'visible', // Ensure it's visible to html2canvas
+        }}
+        className="p-10"
+>
+        {/* Logo and Invoice Title */}
+        <div className="flex justify-between items-center mb-4">
+          <img src={logo} alt="Company Logo" className="w-32 h-auto" /> {/* Adjust size as necessary */}
+          <h1 className="text-4xl font-bold tracking-wide text-right">PENDING INVOICE</h1>
+        </div>
+
+        {/* Customer Information */}
+        <div className="flex justify-between mt-10">
+          <div className="text-left">
+            <p><strong>ISSUED TO:</strong></p>
+            <p>{selectedRowData.customerName || 'Customer Name'}</p>
+            <p>{selectedRowData.customerAddress || 'Customer Address'}</p>
+            <p>{selectedRowData.customerContact || 'Customer Contact'}</p>
+          </div>
+
+          <div className="text-right">
+            <p><strong>INVOICE ID:</strong> {selectedRowData._id || '01234'}</p>
+            <p><strong>DATE:</strong> {selectedRowData.invoiceDate || '11.02.2030'}</p>
+            <p><strong>DUE DATE:</strong> {selectedRowData.dueDate || '11.03.2030'}</p>
+          </div>
+        </div>
+
+        {/* Shipping and Payment Information */}
+        <div className="mt-8">
+          <p><strong>PAY TO:</strong></p>
+          <p>Shipping Method: {selectedRowData.shippingMethod || 'Standard Shipping'}</p>
+          <p>Delivery Date: {selectedRowData.deliveryDate || '11.04.2030'}</p>
+        </div>
+
+        {/* Order Details Table */}
         <div className="mt-10">
           <table className="w-full text-left table-auto border-collapse">
             <thead>
@@ -237,7 +359,19 @@ function pendingInvoice() {
 
         {/* Subtotal, Discounts, and Total */}
         <div className="mt-8">
+          <div className="grid grid-cols-2 gap-2">
+            <div className="text-right">
+              <p><strong>SUBTOTAL:</strong></p>
+              <p><strong>DISCOUNTS:</strong></p>
+            </div>
+            <div className="text-right">
+              <p>{formatCurrency(selectedRowData.subTotal || 0)}</p>
+              <p>{formatCurrency(selectedRowData.discounts || 0)}</p>
+            </div>
+          </div>
+
           <div className="border-t-2 border-gray-300 my-2"></div>
+
           <div className="grid grid-cols-2 gap-2">
             <div className="text-right">
               <p className="text-lg font-bold"><strong>TOTAL AMOUNT:</strong></p>
@@ -247,14 +381,30 @@ function pendingInvoice() {
             </div>
           </div>
         </div>
+
+        {/* Terms and Notes */}
+        <div className="mt-10">
+          <p><strong>TERMS AND CONDITIONS:</strong></p>
+          <p>{selectedRowData.terms || 'Default terms and conditions.'}</p>
+        </div>
+
+        <div className="mt-4">
+          <p><strong>NOTES:</strong></p>
+          <p>{selectedRowData.notes || 'Additional notes.'}</p>
+        </div>
       </div>
+    </div>
+
+
+
+
       <div className="text-center mt-4">
           <button
             type="button"
             className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700 transition-colors"
-            onClick={confirmSubmission} // Trigger submission
+            onClick={handleGeneratePdf} // Trigger submission
           >
-            Submit Invoice
+            Download Preview
           </button>
       </div>
             <button
